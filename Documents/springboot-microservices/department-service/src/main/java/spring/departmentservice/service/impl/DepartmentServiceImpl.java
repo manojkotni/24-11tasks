cc56bd42b7ab97ea8954a0340b3a0f8323b347fc
kxm020302 <kxm020302@darden.com>
2023-11-26 22:41:04 +0530

    commit

---
+++ Documents/springboot-microservices/department-service/src/main/java/spring/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,40 @@
+package spring.departmentservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import spring.departmentservice.dto.DepartmentDto;
+import spring.departmentservice.entity.Department;
+import spring.departmentservice.mapper.DepartmentMapper;
+import spring.departmentservice.repository.DepartmentRepository;
+import spring.departmentservice.service.DepartmentService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+
+        // convert department dto to department jpa entity
+        Department department = DepartmentMapper.mapToDepartment(departmentDto);
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = DepartmentMapper.mapToDepartmentDto(savedDepartment);
+
+        return savedDepartmentDto;
+    }
+
+    @Override
+    public DepartmentDto getDepartmentByCode(String departmentCode) {
+
+        Department department = departmentRepository.findByDepartmentCode(departmentCode);
+
+        DepartmentDto departmentDto = DepartmentMapper.mapToDepartmentDto(department);
+
+        return departmentDto;
+    }
+}
